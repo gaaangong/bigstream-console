a0b568928889c7fa413524ed1c3b14d1e50b6455
gangnobi <gangnobi@gmail.com>
2017-07-07 11:53:19 +0700

    jobeditor css style fixed

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var server = express()
+var path = require('path');
+server.use('/', express.static(path.join(__dirname,'app')))
+server.use('/bower_components', express.static(path.join(__dirname,'bower_components')))
 
-server.use(express.static('app'))
-server.use('/bower_components', express.static('bower_components'))
 
-
-server.get('*', function (req, res) {
-  res.sendFile(__dirname + '/app/index.html')
+server.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname,'/app/index.html'))
 })
 
-server.listen(3000)
+server.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
