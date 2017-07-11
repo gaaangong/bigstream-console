0be197924cc60177fe4d45a94700584b5a9058ea
gangnobi <gangnobi@gmail.com>
2017-07-11 17:03:49 +0700

    add action save,save as,close in Jobeditor

--- main.js
+++ main.js
@@ -13,7 +13,11 @@ let win
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({
+    "width": 800, "height": 600, "webPreferences": {
+      "webSecurity": false
+    }
+  })
   // and load the index.html of the app.
   win.loadURL('http://localhost:3000')
 
