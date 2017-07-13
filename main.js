928417b3c8df4c7c52f48f635825813a9da803e2
gangnobi <gangnobi@gmail.com>
2017-07-13 17:14:19 +0700

    added call plugin manage & add tree page

--- main.js
+++ main.js
@@ -14,15 +14,17 @@ let win
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
-    "width": 800, "height": 600, "webPreferences": {
+    "width": 1024, "height": 600, "webPreferences": {
       "webSecurity": false
-    }
+    },
+    'minHeight': 600,
+    'minWidth': 1024
   })
   // and load the index.html of the app.
   win.loadURL('http://localhost:3000')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
--- modules/config.js
+++ modules/config.js
@@ -3,8 +3,8 @@ const store = new Store();
 
 if (!store.get('api')) {
   store.set({
-    'api.job_api': 'http://10.226.63.52:19980/v1/jobs',
-    'api.storages_api': 'http://bigmaster.igridproject.info:19080/v0.1/storage',
-    'api.storages_obj_api': 'http://bigmaster.igridproject.info:19080/v0.1/object'
+    'api.job_api': 'http://localhost:19980/v1/jobs',
+    'api.storages_api': 'http://localhost:19080/v0.1/storage',
+    'api.storages_obj_api': 'http://localhost:19080/v0.1/object'
   });
 }
