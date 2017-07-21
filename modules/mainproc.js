675a386df2e9bf62e6e29fb6bea5066c48c17bb2
gangnobi <gangnobi@gmail.com>
2017-07-21 11:29:12 +0700

    add log file write to app data dir

--- modules/mainproc.js
+++ modules/mainproc.js
@@ -1,5 +1,8 @@
+const { app } = require('electron')
 const ipc = require('electron').ipcMain
 const dialog = require('electron').dialog
+var fs = require('fs');
+var util = require('util');
 
 ipc.on('open-error-dialog', function (event, msg) {
     dialog.showErrorBox(msg.title, msg.detail)
@@ -18,7 +21,14 @@ ipc.on('save-dialog', function (event, options) {
             return;
         }
         // fileName is a string that contains the path and filename created in the save file dialog.  
-        
+
         event.sender.send('saved-file', fileName)
     })
+})
+
+// create a file only file logger
+const log = require('simple-node-logger').createSimpleLogger(app.getPath('appData') + '/' + app.getName() + '/log.txt');
+ipc.on('log', function (event, msg) {
+    if (msg.method === 'log') msg.method = 'info';
+    log[msg.method](msg.text);
 })
